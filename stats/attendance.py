49d2cc50b817d6c214b5257d4e502a5fe6cf2eed
ddintzner <ddintzner@gmail.com>
2019-03-02 20:39:35 -0800

    Completed Module2

--- stats/attendance.py
+++ stats/attendance.py
@@ -8,10 +8,10 @@ attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendan
 attendance.columns = ['year', 'attendance']
 
 attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
-attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
+attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
 
 plt.xlabel('Year')
-plt.ylabel('attendance')
+plt.ylabel('Attendance')
 plt.axhline(y=attendance['attendance'].mean(), label="Mean", linestyle='--', color='green')
 
-plt.show
+plt.show()
